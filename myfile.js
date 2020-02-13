f074ee4a7e66f4ee721a326695777d1dfa7ae7f2
Hazoor Singh Sidhu <765147@student.tor.loc>
2020-02-13 11:44:51 -0500

    1144 commit

--- myfile.js
+++ myfile.js
@@ -1,5 +1,12 @@
 document.write("Hello World");
 var a1=10;
-var a2=20;
+var a2="Tiger";
 var a3=a1+a2;
+
 document.write(a3);
+var myvar="global";
+    function checkscope()
+    {
+        var myvar="local";
+        document.write(myvar);
+    }
